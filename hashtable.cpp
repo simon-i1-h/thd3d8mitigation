6c2607528e9e8e25ba33ade035e98fd9d1daac3f
Itoh Shimon <shimon@postfixnotation.org>
2020-08-14 12:59:54 +0900

    ハッシュテーブルを追加。オブジェクトの拡張情報の管理に使用する。

---
+++ hashtable.cpp
@@ -0,0 +1,34 @@
+#include <unordered_map>
+
+// XXX TODO exception safety
+
+using htpair = std::pair<uintptr_t, void*>;
+
+struct hashtable {
+	std::unordered_map<uintptr_t, void*> map;
+};
+
+extern "C" struct hashtable* hashtable_new(void)
+{
+	return new struct hashtable;
+}
+
+extern "C" void hashtable_del(struct hashtable* h)
+{
+	delete h;
+}
+
+extern "C" void hashtable_insert(struct hashtable* h, uintptr_t k, void* v)
+{
+	h->map.insert(htpair{ k, v });
+}
+
+extern "C" void hashtable_erase(struct hashtable* h, uintptr_t k)
+{
+	h->map.erase(k);
+}
+
+extern "C" void* hashtable_get(struct hashtable* h, uintptr_t k)
+{
+	return h->map[k];
+}
--- thd3d8fix.h
+++ thd3d8fix.h
@@ -7,6 +7,12 @@
 
 #define THF_LOG_PREFIX "[thd3d8fix]"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// util.c
+
 int myvasprintf(char**, const char*, va_list);
 int myasprintf(char**, const char*, ...);
 int AllocateErrorMessageA(DWORD, char**);
@@ -14,3 +20,17 @@ void ThfVLog(const char*, va_list);
 void ThfLog(const char*, ...);
 void ThfVError(DWORD, const char*, va_list);
 void ThfError(DWORD, const char*, ...);
+
+// hashtable.cpp
+
+struct hashtable;
+
+struct hashtable* hashtable_new(void);
+void hashtable_del(struct hashtable* h);
+void hashtable_insert(struct hashtable* h, uintptr_t k, void* v);
+void hashtable_erase(struct hashtable* h, uintptr_t k);
+void* hashtable_get(struct hashtable* h, uintptr_t k);
+
+#ifdef __cplusplus
+}
+#endif
