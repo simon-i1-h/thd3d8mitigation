73e1b176f1a04e97cfd83a3fb41835b996960b4a
Itoh Shimon <shimon@postfixnotation.org>
2020-11-15 13:48:55 +0900

    版

--- include/thd3d8mitigationversion.h
+++ include/thd3d8mitigationversion.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#define PROGRAM_VERSION "1.10dev"
+#define PROGRAM_VERSION "1.10"
 
 /* for resource script */
-#define PROGRAM_TITLE "thd3d8mitigation v1.10dev"
+#define PROGRAM_TITLE "thd3d8mitigation v1.10"
